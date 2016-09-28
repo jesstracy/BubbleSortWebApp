ac47ee9c33493dcc1feea43cdf4fdd19fa0ef32c
Jessica Tracy <j.tracy916@gmail.com>
2016-09-28 12:56:28 -0400

    Initial commit

---
+++ src/main/java/com/tiy/BubbleSorterWebAnimationApplication.java
@@ -0,0 +1,12 @@
+package com.tiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleSorterWebAnimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleSorterWebAnimationApplication.class, args);
+	}
+}
