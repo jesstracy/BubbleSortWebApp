b788948a69a750dc07966a8431b770e6f3cf5f36
Jessica Tracy <j.tracy916@gmail.com>
2016-09-28 20:40:33 -0400

    Correctly stepping through first row

--- src/main/resources/static/js/bubblesorter-ng-controller.js
+++ src/main/resources/static/js/bubblesorter-ng-controller.js
@@ -10,6 +10,7 @@ angular.module('BubbleSorterAngularApp', [])
                         console.log(response.data);
                         console.log("Adding data to scope");
                         $scope.listOfSteps = response.data;
+                        $scope.currentArray = $scope.listOfSteps[currentOuterIndex];
                     },
                     function errorCallback(response) {
                         console.log("Unable to get data...");
@@ -17,9 +18,49 @@ angular.module('BubbleSorterAngularApp', [])
         };
 
 
+//        $scope.stepThroughButton = function() {
+////            $scope.currentArray = $scope.listOfSteps[currentOuterIndex];
+////            if (!(currentOuterIndex >= $scope.listOfSteps.length)) {
+////                currentOuterIndex++;
+////            }
+//            console.log("In stepThrough Button method");
+//            console.log("Current int: " + $scope.listOfSteps[currentOuterIndex][currentInnerIndex]);
+//            $scope.currentIntInArray = $scope.listOfSteps[currentOuterIndex][currentInnerIndex];
+//            if (!(currentInnerIndex >= $scope.listOfSteps[currentOuterIndex].length)) {
+//                currentInnerIndex++;
+//            }
+//        }
+
+//        $scope.stepThroughButton = function() {
+//            if ($scope.listOfSteps[currentOuterIndex + 1] != null) {
+//                if ($scope.listOfSteps[currentOuterIndex][currentInnerIndex] != $scope.listOfSteps[currentOuterIndex + 1][currentInnerIndex]) {
+//                    $scope.listOfSteps[currentOuterIndex][currentInnerIndex] = $scope.listOfSteps[currentOuterIndex + 1][currentInnerIndex];
+//                }
+//                $scope.currentArray = $scope.listOfSteps[currentOuterIndex];
+//                if (!(currentInnerIndex >= $scope.listOfSteps[currentOuterIndex].length)) {
+//                    currentInnerIndex++;
+//                } else {
+//                    if (!(currentOuterIndex >= $scope.listOfSteps.length)) {
+//                        currentOuterIndex++;
+//                        $scope.currentArray = $scope.listOfSteps[currentOuterIndex];
+//                    }
+//                }
+//            }
+//        }
+
         $scope.stepThroughButton = function() {
-            $scope.currentArray = $scope.listOfSteps[currentOuterIndex];
-            if (!(currentOuterIndex >= $scope.listOfSteps.length)) {
+            if (!(currentInnerIndex >= $scope.listOfSteps[currentOuterIndex].length)) {
+//                var currentInt = $scope.listOfSteps[currentOuterIndex][currentInnerIndex];
+                if ($scope.listOfSteps[currentOuterIndex][currentInnerIndex] > $scope.listOfSteps[currentOuterIndex][currentInnerIndex + 1]) {
+                    var holder = $scope.listOfSteps[currentOuterIndex][currentInnerIndex];
+                    $scope.listOfSteps[currentOuterIndex][currentInnerIndex] = $scope.listOfSteps[currentOuterIndex][currentInnerIndex + 1];
+                    $scope.listOfSteps[currentOuterIndex][currentInnerIndex + 1] = holder;
+//                    currentInnerIndex +=  2;
+//                } else {
+//                    currentInnerIndex += 1;
+                }
+                currentInnerIndex++;
+            } else if (!(currentOuterIndex >= $scope.listOfSteps.length)) {
                 currentOuterIndex++;
             }
         }
@@ -28,5 +69,6 @@ angular.module('BubbleSorterAngularApp', [])
 
         $scope.color = "black";
         var currentOuterIndex = 0;
+        var currentInnerIndex = 0;
 
     });
